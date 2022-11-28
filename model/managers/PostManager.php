da2a518e54cd75345d874ff1c7ad3b4c0e723560
mayados <mariannedos.cours@gmail.com>
2022-11-28 16:54:34 +0100

    create PostManager php

---
+++ model/managers/PostManager.php
@@ -0,0 +1,34 @@
+<?php
+    namespace Model\Managers;
+    
+    use App\Manager;
+    use App\DAO;
+
+    /* PostManager hérite de la class Manager pour bénéficier des méthodes pré-établies : findAll, findOneById.. */
+    class PostManager extends Manager{
+
+        /* Nom de la class dans le dossier enities */
+        protected $className = "Model\Entities\Post";
+        /* Nom de la table en base de données */
+        protected $tableName = "post";
+
+
+        public function __construct(){
+            parent::connect();
+        }
+
+        /* Méthode pour trouver tous les posts d'un topic */
+        public function findPostsByTopic($id){
+
+            $sql = "SELECT *
+            FROM ".$this->tableName."p
+            WHERE p.topic_id = :id";
+
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id' => $id]),
+                $this->className
+            );
+
+        }
+
+    }
