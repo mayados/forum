a2e760d53cdb1eb02906528118f720cb425c147b
mayados <mariannedos.cours@gmail.com>
2022-11-30 11:49:13 +0100

    delete form

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -14,20 +14,7 @@ foreach($topics as $topic ){
     <?php
 }
 ?>
-    <h4>Ajouter un nouveau topic :</h4>
 
-    
-    <form action="index.php?ctrl=forum&action=" method="post">
-
-        <label for="">Titre du topic :</label>
-        <input type="text">
-
-        <label for="">Message</label>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-
-        <input name="submit" type="submit" value="Envoyer">
-
-    </form>
 
 
 
