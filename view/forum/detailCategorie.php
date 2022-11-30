3ccb1a04c4beb0223c47b77855540286922575e5
mayados <mariannedos.cours@gmail.com>
2022-11-30 11:49:19 +0100

    add form

--- view/forum/detailCategorie.php
+++ view/forum/detailCategorie.php
@@ -1,6 +1,7 @@
 <?php
 
 $topics = $result["data"]['topics'];
+$idCategorie = (isset($_GET["id"])) ? $_GET["id"] : null;
     
 ?>
 
@@ -10,8 +11,24 @@ $topics = $result["data"]['topics'];
 foreach($topics as $topic ){
 
     ?>
-    <p><a href="index.php?ctrl=forum&action=detailTopic&id=<?= $topic->getId()?>"><?=$topic->getTitre()?></a><?= $topic->getDateCreation() ?></p>
+    <p><a href="index.php?ctrl=forum&action=detailTopic&id=<?= $topic->getId()?>"><?=$topic->getTitre()?></a><?= $topic->getDateCreation() ?><?= $topic->getCategorie() ?></p>
 
     <?php
 }
 
+?>
+
+<h4>Ajouter un nouveau topic :</h4>
+
+    
+<form action="index.php?ctrl=forum&action=nouveauTopic&id=<?= $idCategorie ?>" method="post">
+
+    <label for="titre">Titre du topic :</label>
+    <input type="text" id="titre" name="titre">
+
+    <label for="">Message</label>
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+
+    <input name="submit" type="submit" value="Envoyer">
+
+</form>
