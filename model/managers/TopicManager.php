27471343cb2a53fb19842b928283a7aa863ec02b
mayados <115099504+mayados@users.noreply.github.com>
2022-12-02 10:42:03 +0100

    delete former function insertTopic()

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -39,51 +39,10 @@
             /* Plusieurs objets sont attendus car il y a plusieurs topics */
             return $this->getMultipleResults(
                 DAO::select($sql, ['id' => $id]),
+                /* On retourne l'objet, c'est pour ça qu'on ajoute la ligne ci-dessous */
                 $this->className
             );
 
         }
-
-        public function insertTopic($id){
-            /* Il faut d'abord vérifier que ça a été soumis via le formulaire avec les données que l'on souhaite */
-            if(isset($_POST['submit'])){
-
-                $titre = filter_input(INPUT_POST, "titre", FILTER_SANITIZE_SPECIAL_CHARS);
-
-                $texte = filter_input(INPUT_POST, "texte", FILTER_SANITIZE_SPECIAL_CHARS);
-
-
-                /* S'il y a la donnée titre */
-                if($titre){
-
-                    $postManager = new PostManager();
-
-                    /* Voir si les simple quotes ne posent pas des soucis au moment de l'ajout dans la bdd */
-                    $sql = "INSERT INTO
-                    topic(titre,verrouillage,categorie_id,user_id)
-                    VALUES(:titre,'0',:id,'3')";
-
-                    DAO::select($sql,['titre' => $titre,'id'=>$id]);
-                    $this->className;
-
-                    /* Trouver un moyen de mettre le dernier id inséré avec */
-
-                    $insertMessage = "INSERT INTO
-                    post(texte,sujet_id,user_id)
-                    VALUES(:texte,:id,'3')"; 
-
-                    DAO::select($insertMessage,['texte' => $texte,'id'=>$id]);
-                    $this->className;
-
-                    /* Ici, il n'y a pas de return, car on ne veut rien retourner, on veut juste rediriger, étant donné que l'on insert juste en base de données */
-                    header('Location: index.php?ctrl=forum&action=detailCategorie&id='.$id.'');
-                }
-
-
-            }
-
-        }
-
-
 
     }
