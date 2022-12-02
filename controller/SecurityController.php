df76d4c9b0e838e3bca8503c82d3d4a0e689354c
mayados <115099504+mayados@users.noreply.github.com>
2022-12-02 14:50:02 +0100

    add functions directionInscription and directionConnexion, because if we don't redirect to function the server don't find the pages

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -16,30 +16,60 @@
                 return [
                     "view" => VIEW_DIR."home.php"
                 ];
-            }       
+            }     
+
+            public function directionInscription(){
+                return [
+                    "view" => VIEW_DIR."security/register.html"
+                ]; 
+            }
+
             
-            
+            public function directionConnexion(){
+                return [
+                    "view" => VIEW_DIR."security/login.html"
+                ]; 
+            }
+
         public function register(){
+
+
+            $userManager = new UserManager();
             /* On vérifie qu'on reçoit les données saisies dans le formulaire concerné */
+            if(isset($_POST['submit'])){
 
-            /* On filtre les champs de saisie */
+                /* On filtre les champs de saisie */
+                $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_SPECIAL_CHARS);
+                $mail = filter_input(INPUT_POST, "mail", FILTER_VALIDATE_EMAIL);
+                $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+                if($pseudo && $mail && $password){
 
-            /* On vérifie que l'utilisateur n'existe pas (mail) */
+                    /* On vérifie que l'utilisateur n'existe pas (mail) */
+                     if($userManager->mails($mail)==0){
+                          var_dump("non enregistré");
+                     }else{
+                        var_dump("deja enregistre");
+                     }
 
-            /* On vérifie que le pseudo n'existe pas */
+                     /* On vérifie que le pseudo n'existe pas */
 
-            /* On vérifie que les deux password correspondent */
+                     /* On vérifie que les deux password correspondent */
 
-            /* On hash le password */
+                     /* On hash le password */
 
-            /* On ajoute l'user en bdd */
+                     /* On ajoute l'user en bdd */
 
-            /* Redirection dans la foulée vers le formulaire de connexion */
-            return [
-                "view" => VIEW_DIR."forum/login.html"
-            ];
+                     /* Redirection dans la foulée vers le formulaire de connexion */
+
+                 }
+
+
+                return [
+                    "view" => VIEW_DIR."/security/login.html"
+                ];
+
+            }
 
         }
-   
 
-    }
+ }
