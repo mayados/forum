6d513a150a86c97f38ee236ed60e7ebffc7b6c0c
mayados <115099504+mayados@users.noreply.github.com>
2022-12-02 15:49:29 +0100

    all the verifications done

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -42,17 +42,18 @@
                 $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_SPECIAL_CHARS);
                 $mail = filter_input(INPUT_POST, "mail", FILTER_VALIDATE_EMAIL);
                 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
-                if($pseudo && $mail && $password){
+                $password2 = filter_input(INPUT_POST, "password2", FILTER_SANITIZE_SPECIAL_CHARS);
 
-                    /* On vérifie que l'utilisateur n'existe pas (mail) */
-                     if($userManager->mails($mail)==NULL){
-                        var_dump($userManager->mails($mail));
-                          var_dump("non enregistré");
+                if($pseudo && $mail && $password && $password2){
+
+                    /* On vérifie que l'utilisateur n'existe pas (mail) && que le pseudo n'existe pas*/
+                     if($userManager->mails($mail)==NULL && $userManager->pseudos($pseudo)==NULL && $password2===$password){
+                       /* Si toutes les conditions du dessus sont vérifiées, on hash le mot de pass (le 1er car le 2eme sert uniquement à la comparaison) avec un algo (2eme parametre de la function) */
+                        password_hash($password,PASSWORD_BCRYPT);
+                        var_dump("bien joué");
                      }else{
-                        var_dump("deja enregistre");
+                        var_dump("les mots de passe ne sont pas les mêmes");
                      }
-
-                     /* On vérifie que le pseudo n'existe pas */
 
                      /* On vérifie que les deux password correspondent */
 
