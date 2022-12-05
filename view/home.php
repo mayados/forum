a8118aaf4d42a5186e8f1431dd0494a542af4e9b
mayados <mariannedos.cours@gmail.com>
2022-12-05 15:52:13 +0100

    add if getUser SESSION

--- view/home.php
+++ view/home.php
@@ -2,8 +2,19 @@
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
-<p>
-    <a href="view/security/login.html">Se connecter</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="view/security/register.html">S'inscrire</a>
-</p>
+<?php
+    /* S'il y a déjà un utilisateur dans la $_SESSION, on affiche Déconnexion */
+    if(App\Session::getUser()){
+    ?>
+        <a href="index.php?ctrl=security&action=viewProfile"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
+        <a href="index.php?ctrl=security&action=deconnexion">Déconnexion</a>                        
+        <?php
+    }else{
+        /* S'il n'y a pas d'utilisateur en SESSION, on propose de se connecter ou s'inscrire */
+        ?>
+        <a href="view/security/login.html">Se connecter</a>
+        <span>&nbsp;-&nbsp;</span>
+        <a href="view/security/register.html">S'inscrire</a>       
+    <?php
+    }
+?>
