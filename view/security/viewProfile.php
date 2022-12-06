2de260e5f654f629a07502d92da48ccbbc7d78e0
mayados <mariannedos.cours@gmail.com>
2022-12-06 14:59:07 +0100

    add php to get topics created by the user

--- view/security/viewProfile.php
+++ view/security/viewProfile.php
@@ -1,7 +1,22 @@
-
+<?php 
+/* S'il y a des datas... */
+$topics = $result["data"]['topics'];
+?>
 <h4>Mon profil</h4>
 
 <p>Pseudo : <?= $_SESSION["user"]->getPseudo() ?></p>
 
 <p>Adresse e-mail : <?= $_SESSION["user"]->getMail() ?></p>
+
+<p>Mes sujets : </p>
+<div id="mes-sujets">
+<?php 
+    foreach($topics as $topic){
+    ?>
+    <p><?=  $topic->getTitre()?></p>
+    <?php
+    }
+?>
+</div>
+
 
