2c20befca4dc528c1fc8f943f251fe767d58e238
mayados <mariannedos.cours@gmail.com>
2022-12-06 09:50:48 +0100

    add div to structure

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -6,19 +6,23 @@ $categories = $result["data"]['categories'];
     
 ?>
 
-<h1>liste catégories</h1>
-
-<?php
-/* Pour chaque data de categories qu'on appelle $categorie, on appelle la méthode qui nous intéresse présente dans l'entities Categorie */
-foreach($categories as $categorie ){
-    /* On fait appelle à la méthode getNomCategorie de l'entité Categorie */
-    ?>
-    <div>
-        <a href="index.php?ctrl=forum&action=detailCategorie&id=<?= $categorie->getId()?>"><?=$categorie->getNomCategorie()?></a>
-    </div>
-
+<div id="main-listcategories">
+    <h1>Liste des catégories</h1>
+    <div class="categories">
     <?php
-}
+    /* Pour chaque data de categories qu'on appelle $categorie, on appelle la méthode qui nous intéresse présente dans l'entities Categorie */
+    foreach($categories as $categorie ){
+        /* On fait appelle à la méthode getNomCategorie de l'entité Categorie */
+        ?>
+        <div class="categorie-item">
+            <a href="index.php?ctrl=forum&action=detailCategorie&id=<?= $categorie->getId()?>"><?=$categorie->getNomCategorie()?></a>
+        </div>
+        <?php
+    }
+    ?>
+    </div>    
+</div>
+
 
 
 
