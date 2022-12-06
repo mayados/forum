af383b269fb5e4b7596ada6990576c24ccb89acb
mayados <mariannedos.cours@gmail.com>
2022-12-06 09:50:15 +0100

    change link in href

--- view/layout.php
+++ view/layout.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-    <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+    <?php /* Il faut diriger vers public, car la redirection de base ne fonctionne pas  */ ?>
+    <link rel="stylesheet" href="public/css/style.css">
     <title>FORUM</title>
 </head>
 <body>
@@ -37,7 +38,8 @@
                             ?>
                             <a href="index.php?ctrl=security&action=viewProfile"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
                             <a href="index.php?ctrl=security&action=deconnexion">Déconnexion</a>
-                            <a href="index.php?ctrl=forum&action=listTopics">la liste des topics</a>                            
+                            <a href="index.php?ctrl=forum&action=listTopics">la liste des topics</a> 
+                            <a href="index.php?ctrl=forum&action=listCategories">la liste des Categories</a>                             
                             <?php
                         }
                         else{
