aae1698e03664beb6dc37434bba422135ed43397
mayados <115099504+mayados@users.noreply.github.com>
2022-12-08 16:47:24 +0100

    delete useless spaces

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -12,8 +12,6 @@
     class HomeController extends AbstractController implements ControllerInterface{
 
         public function index(){
-            
-           
                 return [
                     "view" => VIEW_DIR."home.php"
                 ];
