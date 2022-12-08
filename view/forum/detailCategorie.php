813841a5f9c11f3735767d9b7ef5b6d8eeaeb410
mayados <115099504+mayados@users.noreply.github.com>
2022-12-08 11:55:11 +0100

    add condition : if there are no topics, a message appears / + create condition verrouillage

--- view/forum/detailCategorie.php
+++ view/forum/detailCategorie.php
@@ -1,6 +1,8 @@
 <?php
 
 $topics = $result["data"]['topics'];
+
+if(!$topics==null){
 /* On peut récupérer l'id de la catégorie grâce à l'id placé dans le lien de la  page listCategories.php */
 $idCategorie = (isset($_GET["id"])) ? $_GET["id"] : null;
     
@@ -29,6 +31,19 @@ $idCategorie = (isset($_GET["id"])) ? $_GET["id"] : null;
                         ?>
                 </p>  
                 <p><?= $topic->getDateCreation() ?> </p> 
+                <?php
+                    if(App\Session::isAdmin()){
+                        if($topic->getVerrouillage()==0){
+                ?>
+                    <a href="index.php?ctrl=security&action=closeTopic&id=<?= $topic->getId()?>&categorie=<?= $idCategorie ?>">Clore</a>
+                <?php
+                        }else{
+                        ?>
+                            <p>Sujet clos</p>
+                        <?php
+                        }
+                    }
+                ?>
             </a> 
             <?php
         }
@@ -55,7 +70,30 @@ $idCategorie = (isset($_GET["id"])) ? $_GET["id"] : null;
         </form>             
     </div>
 </div>
+<?php
+}else{
+    ?>
+    <h2>Pas de topics pour le moment...</h2>
+    <div id="premier-topic">
+        <h4>Créer le 1er topic :</h4>
 
+        <form id="form-topic" action="index.php?ctrl=forum&action=nouveauTopic&id=<?= $idCategorie ?>" method="post">
 
+            <div class="form-field">
+                <label for="titre">Titre du topic :</label>
+                <input type="text" id="titre" name="titre" maxlength="80" required>                
+            </div>
+            <div class="form-field">
+                <label for="texte">Message</label>
+                <textarea name="texte" id="texte" cols="30" rows="10" required></textarea>                
+            </div>
 
-
+            <div class="form-field">
+                 <input class="form-button" name="submit" type="submit" value="Envoyer">                
+            </div>
+        </form>             
+    </div>
+</div>
+    <?php
+}
+?>
