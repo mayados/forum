ee63d0e38a6b1b4f49ece9bac2179b2d015a3e72
mayados <115099504+mayados@users.noreply.github.com>
2022-12-09 16:24:59 +0100

    add function findLastTopics()

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -45,6 +45,22 @@
 
         }
 
+        public function findLastTopics($id){
+            
+            $sql = "SELECT *
+            FROM ".$this->tableName."
+            WHERE user_id = :id
+            ORDER BY dateCreation DESC
+            LIMIT 5";
+
+            /* Plusieurs objets sont attendus car il y a plusieurs topics */
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id' => $id]),
+                /* On retourne l'objet, c'est pour ça qu'on ajoute la ligne ci-dessous */
+                $this->className
+            );
+        }
+
         public function findTopicsByUser($id){
 
             $sql = "SELECT titre,id_topic, user_id, verrouillage, dateCreation
