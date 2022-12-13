0be57352399ed5c54712cd7a43636aad26143795
mayados <115099504+mayados@users.noreply.github.com>
2022-12-13 12:00:20 +0100

    restructure + add style css

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -4,40 +4,50 @@ $topics = $result["data"]['topics'];
     
 ?>
 
-<h1>liste topics</h1>
-
-<?php
-foreach($topics as $topic ){
-    ?>
-    <p><a href="index.php?ctrl=forum&action=detailTopic&id=<?= $topic->getId()?>"><?=$topic->getTitre()?></a> (sujet de : 
-        <?php
-            if($topic->getUser()==false){
-            ?>
-            Utilisateur banni
+<div id="main-listTopics">
+    <div id="topics-list">
+        <h1>liste topics</h1>
             <?php
-            }else{
-        ?>
-        <a href="index.php?ctrl=security&action=viewProfileUsers&id=<?= $topic->getUser()->getId() ?>"><?=$topic->getUser()->getPseudo()?></a>)
-        <?php
-            }
-        if(App\Session::isAdmin()){
-            if($topic->getVerrouillage()==NULL){
+            foreach($topics as $topic ){
                 ?>
-                    <a href="index.php?ctrl=security&action=closeTopic&id=<?= $topic->getId()?>">Clore</a>
+                <div class="topic-element">
+                    <p><a href="index.php?ctrl=forum&action=detailTopic&id=<?= $topic->getId()?>"><?=$topic->getTitre()?></a></p>
+
+                        <?php
+                            if($topic->getUser()==false){
+                            ?>
+                                <p>Utilisateur banni</p>
+                            <?php
+                            }else{
+                        ?>
+                        <a href="index.php?ctrl=security&action=viewProfileUsers&id=<?= $topic->getUser()->getId() ?>"><?=$topic->getUser()->getPseudo()?></a>
+                        <?php
+                            }
+                        if(App\Session::isAdmin()){
+                            if($topic->getVerrouillage()==NULL){
+                                ?>
+                                    <p><a href="index.php?ctrl=security&action=closeTopic&id=<?= $topic->getId()?>">Clore</a></p>
+                                <?php
+                            }else{
+                                ?>
+                                    <p>Sujet clos</p>
+                                <?php
+                            }
+                        }
+                        ?>
+       
+                </div>
+
                 <?php
-            }else{
-                ?>
-                    <p>Sujet clos</p>
-                <?php
+                /* Quand on fait un var_dump de $topic->getUser, on voit qu'il récupère l'objet associé (user), il n'y a donc plus qu'à mettre la propriété que nous souhaitons à partir de cet objet (ici ->getPseudo()) */
+                // var_dump($topic->getUser());
             }
-        }
-        ?>
-    </p>
-    <?php
-    /* Quand on fait un var_dump de $topic->getUser, on voit qu'il récupère l'objet associé (user), il n'y a donc plus qu'à mettre la propriété que nous souhaitons à partir de cet objet (ici ->getPseudo()) */
-    // var_dump($topic->getUser());
-}
-?>
+            ?>            
+    </div>
+   
+</div>
+
+
 
 
 
