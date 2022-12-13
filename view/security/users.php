7569696a7047f947d0ee39443d35beda0e7f5370
mayados <115099504+mayados@users.noreply.github.com>
2022-12-13 15:44:04 +0100

    restructure and add style

--- view/security/users.php
+++ view/security/users.php
@@ -6,18 +6,32 @@ $users = $result["data"]['users'];
     
 ?>
 
-<h1>liste des membres</h1>
+<div id="main-users">
+    <h1>liste des membres</h1>
 
-<?php
-/* Pour chaque data de categories qu'on appelle $categorie, on appelle la méthode qui nous intéresse présente dans l'entities Categorie */
-foreach($users as $user ){
-    /* On fait appelle à la méthode getNomCategorie de l'entité Categorie */
-    ?>
-    <div>
-        <p><?=$user->getPseudo()?><a href="index.php?ctrl=security&action=ban&id=<?= $user->getId() ?>">Bannir</a></p>
+    <div id="list-users">
+        <?php
+        /* Pour chaque data de categories qu'on appelle $categorie, on appelle la méthode qui nous intéresse présente dans l'entities Categorie */
+        foreach($users as $user ){
+            /* On fait appelle à la méthode getNomCategorie de l'entité Categorie */
+            ?>
+            <div class="user">
+                <div class="user-pseudo">
+                    <a href="index.php?ctrl=security&action=viewProfileUsers&id=<?= $user->getId() ?>" ><?=$user->getPseudo()?></a>
+                </div>  
+                <div class="user-date">
+                    <p><?=$user->getDateInscription()?></p>
+                </div>
+                <div class="ban-user">
+                    <a href="index.php?ctrl=security&action=ban&id=<?= $user->getId() ?>">Bannir <i class="fa-solid fa-ban"></i></a>                    
+                </div>              
+            </div>
+            <?php
+        }    
+        ?>        
     </div>
 
-    <?php
-}
+</div>
+
 
 
