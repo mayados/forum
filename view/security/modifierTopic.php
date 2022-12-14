3ff3de923abaf4075579ff3c1357612fec8ab1e9
mayados <115099504+mayados@users.noreply.github.com>
2022-12-14 12:07:06 +0100

    create page for modifierTopic

---
+++ view/security/modifierTopic.php
@@ -0,0 +1,32 @@
+<?php
+
+$topic = $result["data"]['topic'];
+$post = $result["data"]['post'];
+var_dump($post->getTexte());
+
+if($_SESSION["user"]->getId() == $topic->getUser()->getId()){
+?>
+
+    <div id="ajout-topic">
+        <h4>Modifier le topic :</h4>
+
+        <form id="form-topic" action="index.php?ctrl=security&action=updateTopic&id=<?= $topic->getId()?>" method="post">
+
+            <div class="form-field">
+                <label for="titre">Titre du topic :</label>
+                <input type="text" id="titre" name="titre" maxlength="80" value="<?= $topic->getTitre() ?>" required>
+            </div>
+            <div class="form-field">
+                <label for="texte">Message</label>
+                <textarea name="texte" id="texte" cols="30" rows="10" required><?= $post->getTexte() ?></textarea>
+            </div>
+
+            <div class="form-field">
+                <input class="form-button" name="submit" type="submit" value="Envoyer">
+            </div>
+        </form>
+    </div>
+
+<?php
+}
+?>
