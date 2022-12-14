8077eb4cb23aca9f224d989124e95b32635b669e
mayados <115099504+mayados@users.noreply.github.com>
2022-12-14 12:05:36 +0100

    add function deletePostsByTopic() / firstPostByTopic() / updateFirstPostTopic()

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -20,7 +20,7 @@
         /* Méthode pour trouver tous les posts d'un topic */
         public function findPostsByTopic($id){
 
-            $sql = "SELECT texte, dateCreation, user_id, topic_id
+            $sql = "SELECT texte, dateCreation, user_id, topic_id, id_post
             FROM ".$this->tableName."
             WHERE topic_id = :id
             ORDER BY dateCreation ASC";
@@ -44,6 +44,49 @@
             /* Plusieurs objets sont attendus car il y a plusieurs topics */
             return $this->getMultipleResults(
                 DAO::select($sql, ['id' => $id]),
+                /* On retourne l'objet, c'est pour ça qu'on ajoute la ligne ci-dessous */
+                $this->className
+            );
+
+        }
+
+        public function deletePostsByTopic($id){
+
+            $sql = "DELETE FROM ".$this->tableName."
+            WHERE topic_id = :id";
+
+            return $this->getSingleScalarResult(
+                DAO::select($sql, ['id' => $id]),
+                /* On retourne l'objet, c'est pour ça qu'on ajoute la ligne ci-dessous */
+                $this->className
+            );
+
+        }
+
+        public function firstPostByTopic($id){
+
+            $sql = "SELECT *
+                    FROM ".$this->tableName." 
+                    WHERE topic_id = :id
+                    ORDER BY dateCreation ASC
+                    LIMIT 1
+                    ";
+
+            return $this->getOneOrNullResult(
+                DAO::select($sql, ['id' => $id], false), 
+                $this->className
+            );
+
+        }
+
+        public function updateFirstPostTopic($texte){
+
+            $sql = "UPDATE ".$this->tableName."
+            SET texte = :texte
+            WHERE topic_id = 94";
+
+            return $this->getSingleScalarResult(
+                DAO::select($sql, ['texte' => $texte]),
                 /* On retourne l'objet, c'est pour ça qu'on ajoute la ligne ci-dessous */
                 $this->className
             );
